f1eddf29aabbbef211f684bd766c4bc8dbae414a
Omkar Phale <44401060+PhaleOmkar@users.noreply.github.com>
2021-06-04 11:35:34 +0530

    Hello World
    getting started with c programming

---
+++ 01_HelloWorld/HelloWorld.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // header file inclusion
+
+int main(void) // entry point function
+{
+	// code
+	printf("Hello World!\n"); // library function to print output on console/terminal
+	
+	return(0); // returning with success
+} // end of main()
